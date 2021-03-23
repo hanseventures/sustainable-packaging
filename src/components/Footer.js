ee6f813e43348c397a59cc42a5dd938becee6bae
Nelson Rainsborough <37534290+NelRai@users.noreply.github.com>
2021-03-23 13:39:22 +0100

    Footer: list styled, added logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,20 +22,23 @@ const Footer = props => {
   `)
   return (
     <div className="l-footer container bg-primary font-wht">
-      <div className=" content">
-        <h3 className="footer-title">{data.site.siteMetadata.title}</h3>
+      <div className="content content--low">
+        <img
+          className="logo"
+          src="/images/logo/suburban-coworking-logo-white@2x.png"
+        />
         <p>Wir richten Co-Working Spaces in deiner Region ein!</p>
-        <ul>
+        <ul className="list-style-type-none">
           {data.allFooterMenuJson.edges.map(({ node }) => (
-            <li key={node.name}>
+            <li key={node.name} className="text-decoration-underline">
               <Link to={node.url}>{node.name}</Link>
             </li>
           ))}
         </ul>
         <h3 className="font-wht">Kontakt</h3>
         <p>
-          Tel.: 040/ 12345678 <br></br>
-          Mail: <a href="mailto:aaa@aaa.de">info@suburban-coworking.de</a>
+          <b>Tel.:</b> 040/ 12345678 <br></br>
+          <b>Mail:</b> <a href="mailto:info@suburban-coworking.de">info@suburban-coworking.de</a>
         </p>
 
         <small>2021 Copyright - a Hanse Ventures BSJ GmbH Company</small>
