d0ee9bf72014b40ff851bf52ffb44a85de7eaa6d
Peer Falk <peer.falk@hanseventures.com>
2021-05-05 15:30:17 +0200

    add testimonial v2 section

--- src/components/TestimonialsV2.js
+++ src/components/TestimonialsV2.js
@@ -1,76 +1,76 @@
-import React from 'react'
+import React from 'react';
+// import { ReactComponent as StarIcon } from '../../static/svg/star.svg';
 
-const TestimonialsV1 = props => {
+const TestimonialsV2 = props => {
   return (
     <section id="kundenstimmen" className="l-testimonials-v2 container bg-secondary">
 
-      <div className="content content-default d-grid">
+      <div className="content content--roomy trimmed-4 d-grid">
 
-        <div className="wrapper text-xs-center">
+        <h3 className="font-wht text-xs-center">Das sagen unsere Kunden</h3>
 
-          <h3 className="font-wht">Das sagen unsere Kunden</h3>
+        <div className="d-flex row-gap-3 column-gap-3 mt-4 text-xs-center">
 
-          <div className="mb-4 d-flex">
+          <article className="card card--default d-flex flex-basis-full flex-align-center flex-column py-5 px-3">
+            <img src="/images/lvz/lvz-testimonials-02@2x.png" alt="avatar"></img>
 
-            <article className="d-flex flex-align-center flex-column">
-
-              <img src="/images/lvz/lvz-testimonials-02@2x.png" alt="avatar"></img>
-
-              <div className="mb-4 mt-2">
-                <small className="font-xs">
-                  <strong>David van Laak</strong>
-                  <br />
-                  Geschäftsführer
-                </small>
+            <div className="mt-3">
+              <strong>David van Laak</strong>
+              <br />
+              <small>Geschäftsführer</small>
+              <br />
+              <div className="d-flex justify-content-center column-gap-1 mt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M19.63 7.274a1.254 1.254 0 00-1.172-.817h-5.073a.417.417 0 01-.393-.278L11.17 1.02a1.25 1.25 0 00-2.342 0l-.004.013-1.817 5.147a.417.417 0 01-.392.279H1.542a1.25 1.25 0 00-.803 2.209l4.32 3.583a.417.417 0 01.13.453l-1.816 5.442a1.25 1.25 0 001.925 1.403l4.455-3.266a.417.417 0 01.493 0l4.453 3.266a1.25 1.25 0 001.927-1.403l-1.815-5.446a.417.417 0 01.129-.452l4.328-3.589a1.25 1.25 0 00.361-1.384z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M19.63 7.274a1.254 1.254 0 00-1.172-.817h-5.073a.417.417 0 01-.393-.278L11.17 1.02a1.25 1.25 0 00-2.342 0l-.004.013-1.817 5.147a.417.417 0 01-.392.279H1.542a1.25 1.25 0 00-.803 2.209l4.32 3.583a.417.417 0 01.13.453l-1.816 5.442a1.25 1.25 0 001.925 1.403l4.455-3.266a.417.417 0 01.493 0l4.453 3.266a1.25 1.25 0 001.927-1.403l-1.815-5.446a.417.417 0 01.129-.452l4.328-3.589a1.25 1.25 0 00.361-1.384z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M19.63 7.274a1.254 1.254 0 00-1.172-.817h-5.073a.417.417 0 01-.393-.278L11.17 1.02a1.25 1.25 0 00-2.342 0l-.004.013-1.817 5.147a.417.417 0 01-.392.279H1.542a1.25 1.25 0 00-.803 2.209l4.32 3.583a.417.417 0 01.13.453l-1.816 5.442a1.25 1.25 0 001.925 1.403l4.455-3.266a.417.417 0 01.493 0l4.453 3.266a1.25 1.25 0 001.927-1.403l-1.815-5.446a.417.417 0 01.129-.452l4.328-3.589a1.25 1.25 0 00.361-1.384z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M19.63 7.274a1.254 1.254 0 00-1.172-.817h-5.073a.417.417 0 01-.393-.278L11.17 1.02a1.25 1.25 0 00-2.342 0l-.004.013-1.817 5.147a.417.417 0 01-.392.279H1.542a1.25 1.25 0 00-.803 2.209l4.32 3.583a.417.417 0 01.13.453l-1.816 5.442a1.25 1.25 0 001.925 1.403l4.455-3.266a.417.417 0 01.493 0l4.453 3.266a1.25 1.25 0 001.927-1.403l-1.815-5.446a.417.417 0 01.129-.452l4.328-3.589a1.25 1.25 0 00.361-1.384z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <path d="M19.63 7.274a1.254 1.254 0 00-1.172-.817h-5.073a.417.417 0 01-.393-.278L11.17 1.02a1.25 1.25 0 00-2.342 0l-.004.013-1.817 5.147a.417.417 0 01-.392.279H1.542a1.25 1.25 0 00-.803 2.209l4.32 3.583a.417.417 0 01.13.453l-1.816 5.442a1.25 1.25 0 001.925 1.403l4.455-3.266a.417.417 0 01.493 0l4.453 3.266a1.25 1.25 0 001.927-1.403l-1.815-5.446a.417.417 0 01.129-.452l4.328-3.589a1.25 1.25 0 00.361-1.384z"/>
+                </svg>
               </div>
+            </div>
 
-              <blockquote className="mx-3 font-sm">
-                <i>Im Einzelhandel benötigen wir immer neue innovative Verpackungen. XYZ konnte uns da mit ihrem Know-How sehr gut unterstützen.</i>
-              </blockquote>
 
-            </article>
+            <blockquote className="mt-3 mb-0 mx-0">
+              Im Einzelhandel benötigen wir immer neue innovative Verpackungen. XYZ konnte uns da mit ihrem Know-How sehr gut unterstützen.
+            </blockquote>
+          </article>
 
-            <article className="d-flex flex-align-center flex-column">
+          <article className="card card--default d-flex flex-basis-full flex-align-center flex-column py-5 px-3">
+            <img src="/images/lvz/lvz-testimonials-03@2x.png" alt="avatar"></img>
 
-              <img src="/images/lvz/lvz-testimonials-03@2x.png" alt="avatar"></img>
+            <div className="mt-3">
+              <strong>Dörte Waurick</strong>
+              <br />
+              <small>Geschäftsführerin</small>
+            </div>
 
-              <blockquote className="mx-3 font-sm">
-                <i>"Seit nunmehr 53 Jahren ist die LVZ ein verlässlicher Partner, um Neuigkeiten von den akademixern der Leserschaft nahe zu bringen. Danke dafür!"</i>
-              </blockquote>
+            <blockquote className="mt-3 mb-0 mx-0">
+              Seit nunmehr 53 Jahren ist die LVZ ein verlässlicher Partner, um Neuigkeiten von den akademixern der Leserschaft nahe zu bringen. Danke dafür!
+            </blockquote>
+          </article>
 
-              <div className="mb-4 mt-2">
-                <small className="font-xs">
-                  <strong>Dörte Waurick</strong>
-                  <br></br>
-                  Geschäftsführerin <br></br> Kabarett akademixer
-                </small>
-              </div>
+          <article className="card card--default d-flex flex-basis-full flex-align-center flex-column py-5 px-3">
+            <img src="/images/lvz/lvz-testimonials-01@2x.png" alt="avatar"></img>
 
-              <img src="/images/lvz/lvz-testimonial-logo-academixer.jpg" alt="tunici restaurant logo" className="mb-4"></img>
+            <div className="mt-3">
+              <strong>Nico Wehrmann</strong>
+              <br />
+              <small>Inhaber</small>
+            </div>
 
-            </article>
-
-            <article className="d-flex flex-align-center flex-column">
-
-              <img src="/images/lvz/lvz-testimonials-01@2x.png" alt="avatar"></img>
-
-              <blockquote className="mx-3 font-sm">
-                <i>"Wir schätzen die LVZ als Werbepartner sehr. Sie macht, wie auch unser Unternehmen, für viele Leipziger die Zeit zu Hause nach manch turbulentem Tag erst richtig behaglich."</i>
-              </blockquote>
-
-              <div className="mb-4 mt-2">
-                <small className="font-xs">
-                  <strong>Nico Wehrmann</strong>
-                  <br></br>
-                  Inhaber <br></br> Betten Wehrmann
-                </small>
-              </div>
-
-              <img src="/images/lvz/lvz-testimonial-logo-wehmann.jpg" alt="tunici restaurant logo" className="mb-4"></img>
-
-            </article>
-
-          </div>
+            <blockquote className="mt-3 mb-0 mx-0">
+              Wir schätzen die LVZ als Werbepartner sehr. Sie macht, wie auch unser Unternehmen, für viele Leipziger die Zeit zu Hause nach manch turbulentem Tag erst richtig behaglich.
+            </blockquote>
+          </article>
 
         </div>
 
@@ -80,4 +80,4 @@ const TestimonialsV1 = props => {
 
   )
 }
-export default TestimonialsV1
+export default TestimonialsV2
