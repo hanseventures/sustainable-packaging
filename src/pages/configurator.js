84d12d6db18f32cf2ee8b5d2d4ebd5a4acf30756
Peer Falk <peer.falk@hanseventures.com>
2021-05-07 19:25:33 +0200

    fix svg errors

--- src/pages/configurator.js
+++ src/pages/configurator.js
@@ -37,15 +37,15 @@ const Configurator = props => {
 
       <section className="wizard-usp d-flex flex-column flex-md-row justify-content-center font-xs">
         <div className="">
-          <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(0.8333333333333334,0,0,0.8333333333333334,0,0)"><path d="M23.25.749,8.158,22.308a2.2,2.2,0,0,1-3.569.059L.75,17.249" fill="none" stroke="#7EA425" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></g></svg>
+          <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(0.8333333333333334,0,0,0.8333333333333334,0,0)"><path d="M23.25.749,8.158,22.308a2.2,2.2,0,0,1-3.569.059L.75,17.249" fill="none" stroke="#7EA425" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"></path></g></svg>
           <span>Schnelle Angaben zum Produkt machen</span>
         </div>
         <div className="mx-md-3">
-        <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(0.8333333333333334,0,0,0.8333333333333334,0,0)"><path d="M23.25.749,8.158,22.308a2.2,2.2,0,0,1-3.569.059L.75,17.249" fill="none" stroke="#7EA425" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></g></svg>
+        <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(0.8333333333333334,0,0,0.8333333333333334,0,0)"><path d="M23.25.749,8.158,22.308a2.2,2.2,0,0,1-3.569.059L.75,17.249" fill="none" stroke="#7EA425" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"></path></g></svg>
           <span>Einfach und unverbindlich ausfüllen</span>
         </div>
         <div className="">
-        <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(0.8333333333333334,0,0,0.8333333333333334,0,0)"><path d="M23.25.749,8.158,22.308a2.2,2.2,0,0,1-3.569.059L.75,17.249" fill="none" stroke="#7EA425" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></g></svg>
+        <svg viewBox="0 0 20 20" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(0.8333333333333334,0,0,0.8333333333333334,0,0)"><path d="M23.25.749,8.158,22.308a2.2,2.2,0,0,1-3.569.059L.75,17.249" fill="none" stroke="#7EA425" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"></path></g></svg>
           <span>Kostenlose Expertenberatung</span>
         </div>
       </section>
