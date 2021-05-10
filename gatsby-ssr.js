a1128e1241894e675254de060fac531bd5375ec0
jim lesperance <contact@jimlesperance.com>
2021-05-10 12:13:56 +0200

    feat: Updated cookieBar configuration

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <script src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&thirdparty=1&always=1&remember=180&privacyPage=%2Fprivacy%2F" />,
+    <script src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&thirdparty=1&always=1&remember=180&privacyPage=%2Fdatenschutz%2F&forceLang=de&hideDetailsBtn=0&showPolicyLink=1" />,
     <iframe name="hidden_iframe" id="hidden_iframe" className="d-none" />
   ])
 }
