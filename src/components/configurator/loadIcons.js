de80e87d7b476d81a071b3b5e56b45f859b25bf9
jim lesperance <contact@jimlesperance.com>
2021-05-26 23:02:25 +0200

    feat: Added streamlinehq icon lib

--- src/components/configurator/loadIcons.js
+++ src/components/configurator/loadIcons.js
@@ -3,8 +3,8 @@ import splitOnLast from './splitOnLast'
 
 const obj = {};
 context.keys().forEach((key) => {
-  const countryCode = splitOnLast(key, '-').shift().split('./').pop() // remove the first 2 characters
-    .substring(0, key.length - 6); // remove the file extension
+  const countryCode = splitOnLast(key, '-').shift().split('./').pop()
+    .substring(0, key.length - 6);
   obj[countryCode] = context(key);
 });
 
