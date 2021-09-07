9c9d3994f4e41efb437d497642c6e276f8501455
Peer Falk <peer.falk@hanseventures.com>
2021-09-07 14:53:55 +0200

    fix indent

--- src/components/configurator/step10.js
+++ src/components/configurator/step10.js
@@ -40,48 +40,48 @@ const Step10 = props => {
     <>
       { !loading ?
         (<>
-      <h1 className='h-2 text-xs-center'>{configuratorJson.title}</h1>
+          <h1 className='h-2 text-xs-center'>{configuratorJson.title}</h1>
 
-      <div className='progress-bar trimmed-2 mt-4'>
-        <div className={`active step-${configuratorJson.progressbar}`}></div>
-        <div className='non-active'></div>
-      </div>
+          <div className='progress-bar trimmed-2 mt-4'>
+            <div className={`active step-${configuratorJson.progressbar}`}></div>
+            <div className='non-active'></div>
+          </div>
 
-      <MultiSelect
-        id={configuratorJson.id}
-        objects={configuratorJson.nodes}
-        handleSelection={props.handleSelect}
-        selectedObjects={props.selectedObjects}
-        gaName={configuratorJson.gaName}
-      />
+          <MultiSelect
+            id={configuratorJson.id}
+            objects={configuratorJson.nodes}
+            handleSelection={props.handleSelect}
+            selectedObjects={props.selectedObjects}
+            gaName={configuratorJson.gaName}
+          />
 
-      <small className='d-block text-xs-center mb-3'>Mehrfachauswahl möglich</small>
+          <small className='d-block text-xs-center mb-3'>Mehrfachauswahl möglich</small>
 
-      <div className='d-flex flex-column justify-content-center align-self-center p-1 m-1 mb-4 flex-row flex-lg-row'>
-        <div
-          id={`${configuratorJson.gaName}-${configuratorJson.btnPrevious.toLowerCase()}`}
-          className='btn btn-secondary mr-1 mr-md-3'
-          onClick={() => previousStep()}
-          disabled={isLoading || isFirstStep}
-        >
-          {configuratorJson.btnPrevious}
-        </div>
-        <div
-          id={`${configuratorJson.gaName}-${configuratorJson.btnNext.toLowerCase()}`}
-          className={`btn btn-${
-            hasSelected(props.selectedObjects, configuratorJson.id)
-              ? 'primary'
-              : 'secondary'
-          } ml-1 ml-md-3`}
-          //onClick={() => nextStep(Number(activeStep)+1)
-          onClick={() => RedirectHandeler()}
-        >
-          {configuratorJson.btnNext}
-        </div>
-      </div>
-    </>)
-    : <Spinner />}
-</>
+          <div className='d-flex flex-column justify-content-center align-self-center p-1 m-1 mb-4 flex-row flex-lg-row'>
+            <div
+              id={`${configuratorJson.gaName}-${configuratorJson.btnPrevious.toLowerCase()}`}
+              className='btn btn-secondary mr-1 mr-md-3'
+              onClick={() => previousStep()}
+              disabled={isLoading || isFirstStep}
+            >
+              {configuratorJson.btnPrevious}
+            </div>
+            <div
+              id={`${configuratorJson.gaName}-${configuratorJson.btnNext.toLowerCase()}`}
+              className={`btn btn-${
+                hasSelected(props.selectedObjects, configuratorJson.id)
+                  ? 'primary'
+                  : 'secondary'
+              } ml-1 ml-md-3`}
+              //onClick={() => nextStep(Number(activeStep)+1)
+              onClick={() => RedirectHandeler()}
+            >
+              {configuratorJson.btnNext}
+            </div>
+          </div>
+        </>)
+      : <Spinner />}
+    </>
   )
 }
 
